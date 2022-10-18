efcf922fe796f5d7513e045d7ed0036860230bf4
magdy <mohamedmagdy171199@gmail.com>
2022-10-18 16:12:38 +0200

    AddAdminRole

---
+++ Booky Store/Data/Migrations/20221018132736_AddAdminRole.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booky_Store.Data.Migrations
+{
+    public partial class AddAdminRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO [Security].[UserRoles] (UserId,RoleId) SELECT '2238b960-b302-4030-9504-d25f9611b6ae', Id FROM [Security].[Roles]");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [Security].[UserRoles] WHERE UserId='2238b960-b302-4030-9504-d25f9611b6ae'");
+        }
+    }
+}
